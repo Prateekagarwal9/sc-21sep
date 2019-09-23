eb397cd7a29bbb216d7cd160cffd61e3ee114ab1
Prateekagarwal9 <39221904+Prateekagarwal9@users.noreply.github.com>
2019-09-23 09:52:45 +0530

    Update main.bat

--- SupplyChain/databricks_windows/main.bat
+++ SupplyChain/databricks_windows/main.bat
@@ -3,11 +3,12 @@ python -m pip install --upgrade pip --user
 python -m pip install databricks-cli --user
 python -m pip install pexpect --user
 cp d:/local/appdata/python/python36/scripts/databricks.exe .
+cp d:/local/appdata/python/python36/scripts/databricks-script.py .
 cp d:/local/appdata/python/python36/scripts/dbfs.exe .
 git clone https://github.com/Prateekagarwal9/supplychain-new
 python expect.py %1 %2
 databricks workspace delete -r /Supply-Chain-Solution
-databricks workspace import  -f DBC -l SCALA supplychain/Supply-Chain-Solution.dbc /Supply-Chain-Solution
+databricks workspace import  -f DBC -l SCALA supplychain-new/Supply-Chain-Solution.dbc /Supply-Chain-Solution
 rmdir /q /s supplychain-new
 databricks fs rm -r dbfs:/databricks
 databricks fs mkdirs dbfs:/databricks/init/SupplyChain/
